227f832f9fbace59c0b4105e55b805e5394397cf
Ceciliabelemsantos <109307853+Ceciliabelemsantos@users.noreply.github.com>
2022-07-27 14:54:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+ pathImg = loadImage("path.png");
+  boyImg = loadAnimation("jake1.png","jake2.png","jake3.png","jake4.png","jake5.png");
+ 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+path = createSprite(200,200,50,400)
+path.addImage("pathImg",pathImg)
+path.velocityY = -4
+path.scale=1.2;
+
+boy = createSprite(200,350,20,20);
+boy.addAnimation("boyImg",boyImg)
+boy.scale=0.08;
+  
+//crie um limite à esquerda
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false
+//crie um limite à direita
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = Word.mouseX
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+ boy.collide(leftBoundary)
+ boy.collide(rightBoundary)
+  
+  //código para redefinir o fundo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
